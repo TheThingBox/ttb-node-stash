6d625aa5340a7fc46e1f9117b261ab5693f4c6a7
Maxime Journaux <maxime.journaux@digitalairways.com>
2019-01-18 15:50:49 +0100

    init

---
+++ 61-stasher.js
@@ -0,0 +1,98 @@
+module.exports = function(RED) {
+
+	function Stasher(config) {
+		RED.nodes.createNode(this, config);
+
+		this.name = config.name;
+		this.duration = config.duration;
+		this.working = false;
+		this.pile = [];
+
+		this.isWorking = function() { return this.working; }
+		this.work = function() { this.working = true; }
+		this.sleep = function() { this.working = false; }
+		this.stack = function(e, m) { this.pile.push({event: e, msg: m})}
+		this.unstack = function() {
+			var stashed = this.getStash();
+			if(!stashed) return;
+			this.pile.forEach(function(x) {
+				x.msg.payload = stashed;
+				process.nextTick(function() { RED.events.emit(x.event, x.msg); });
+			});
+			this.pile = [];
+		}
+
+		this.getStash = function() {
+			if(!this.stash) return null;
+			if(!this.stash.date) return null;
+			if(!this.stash.payload) return null;
+			var d = this.stash.date+this.duration;
+			if(d > Date.now()) return this.stash.payload;
+			return null;
+		}
+
+		this.setStash = function(payload) {
+			this.stash = {
+				payload: payload,
+				date: Date.now()
+			};
+		}
+	}
+	RED.nodes.registerType("stasher", Stasher);
+
+	function StasherIn(config) {
+		RED.nodes.createNode(this, config);
+
+		this.on("input", function(msg) {
+			var stashout = RED.nodes.getNode(config.out);
+			if(stashout && stashout.stash) {
+				var stashed = stashout.stash.getStash();
+				var event = `node:${stashout.id}`;
+				if(stashed) {
+					msg.payload = stashed;
+					RED.events.emit(event, msg);
+					return;
+				} else if(stashout.stash.isWorking()) {
+					stashout.stash.stack(event, msg);
+					return;
+				}
+				stashout.stash.work();
+			}
+			this.send(msg);
+		});
+	}
+	RED.nodes.registerType("stasher-in", StasherIn);
+
+	function StasherOut(config) {
+		RED.nodes.createNode(this, config);
+
+		this.stash = RED.nodes.getNode(config.stash);
+
+		var event = `node:${config.id}`;
+
+		var handler = function(msg) {
+			console.log("receive event", msg.topic);
+			msg._event = config.event;
+			this.receive(msg);
+		}.bind(this);
+
+		RED.events.on(event, handler);
+
+		this.on("input", function(msg) {
+			console.log("on input", msg.topic);
+			console.log(msg._event, config.event)
+			if(!msg._event || msg._event != config.event) {
+				this.stash.setStash(msg.payload);
+				this.stash.sleep();
+				this.stash.unstack();
+			}
+			this.send(msg);
+		});
+
+		this.on("close", function() {
+			RED.events.removeListener(event, handler);
+		});
+	}
+	RED.nodes.registerType("stasher-out", StasherOut);
+
+}
